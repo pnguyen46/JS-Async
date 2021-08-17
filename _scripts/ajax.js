cd2d47a8776567b3f14ae0b5cfdb6df4c5378140
Tron Nguyen <pnguye894@gmail.com>
2021-08-17 19:59:45 -0400

    Implement async with promises

--- _scripts/ajax.js
+++ _scripts/ajax.js
@@ -1,23 +1,45 @@
 // add global variable containing XHR object here
-let httpRequest = new XMLHttpRequest();
+// let httpRequest = new XMLHttpRequest();
 
 // add get() function here
-function get(url,success,fail) {
-    httpRequest.open("GET",url);
-    httpRequest.onload = () => {
-        if(httpRequest.status === 200) {
-            success(httpRequest.responseText);
-        }else{
-            fail(httpRequest.status);
-        }
-    }
-    httpRequest.send();
+// function get(url,success,fail) {
+//     httpRequest.open("GET",url);
+//     httpRequest.onload = () => {
+//         if(httpRequest.status === 200) {
+//             success(httpRequest.responseText);
+//         }else{
+//             fail(httpRequest.status);
+//         }
+//     }
+//     httpRequest.send();
+// }
+
+function get(url){
+    return new Promise(function(resolve,reject){
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.open("GET",url);
+        httpRequest.onload = function(){
+            if(httpRequest.status === 200) {
+                //Resolve the promise with the response text
+                resolve(httpRequest.responseText);
+            }else{
+                //Reject the promise with the status text
+                reject(Error(httpRequest.statusText));
+            }
+        };
+        //Handle network errors
+        httpRequest.onerror = function(){
+            reject(Error("Network Error"));
+        };
+
+        httpRequest.send();
+    });
 }
 
 function failHandler(status) {
-    console.log('Error code:',status);
-    const weatherDiv = document.querySelector('#weather');
-    weatherDiv.classList.remove('hidden');
+    console.log(status);
+    // const weatherDiv = document.querySelector('#weather');
+    // weatherDiv.classList.remove('hidden');
 
 }
 
@@ -25,11 +47,29 @@ function tempToF(kelvin) {
     return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
 }
 
+// function successHandler(data) {
+//     const dataObj = JSON.parse(data);
+//     const weatherDiv = document.querySelector('#weather');
+//     const weatherFragment = `
+//         <h1>Weather</h1>
+//         <h2 class="top">
+//         <img
+//             src="http://openweathermap.org/img/w/${dataObj.weather[0].icon}.png"
+//             alt="${dataObj.weather[0].description}"
+//             width="50"
+//             height="50"
+//         />${dataObj.name}
+//         </h2>
+//         <p>
+//         <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${dataObj.weather[0].description}
+//         </p>
+//     `
+//     weatherDiv.innerHTML = weatherFragment;
+//     // weatherDiv.classList.remove('hidden');
+// }
 function successHandler(data) {
     const dataObj = JSON.parse(data);
-    const weatherDiv = document.querySelector('#weather');
-    const weatherFragment = `
-        <h1>Weather</h1>
+    const div = `
         <h2 class="top">
         <img
             src="http://openweathermap.org/img/w/${dataObj.weather[0].icon}.png"
@@ -42,14 +82,58 @@ function successHandler(data) {
         <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${dataObj.weather[0].description}
         </p>
     `
-    weatherDiv.innerHTML = weatherFragment;
-    weatherDiv.classList.remove('hidden');
+    return div;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     const apiKey = '06ead3bed8913dd3aee92a6203bd92d9';
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
+    // const apiKey = '';
+    const weatherDiv = document.querySelector('#weather');
+    
+    const locations = [
+        'los+angeles,us',
+        'san+francisco,us',
+        'lone+pine,us',
+        'mariposa,us'
+    ];
+
+    const urls = locations.map(function(location) {
+        return `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`; 
+    });
+
+    // const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
+    
     // add get() function call here
-    get(url,successHandler,failHandler);
+    // get(url,successHandler,failHandler);
     // successHandler(httpRequest.responseText); // will return an error since the data will not come back in time.
+
+    // console.log(get(url));
+
+    // get(url)
+    //     .then(function(response){
+    //         successHandler(response);
+    //     })
+    //     .catch(function(status){
+    //         failHandler(status);
+    //     })
+    //     .finally(function() {
+    //         // const weatherDiv = document.querySelector('#weather');
+    //         weatherDiv.classList.remove('hidden');
+    //     });
+
+    Promise.all([get(urls[0]),get(urls[1]),get(urls[2]),get(urls[3])])
+            .then(function(responses){
+                return responses.map(function(response){
+                    return successHandler(response);
+                });
+            })
+            .then(function(literals){
+                weatherDiv.innerHTML = `<h1>Weather</h1>${literals.join('')}`;
+            })
+            .catch(function(status){
+                failHandler(status);
+            })
+            .finally(function(){
+                weatherDiv.classList.remove('hidden');
+            });
 });
